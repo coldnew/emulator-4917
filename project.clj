a6f9377e845425184c9785a244d9f8f9981df8e4
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-06-28 16:59:11 +0800

    project.clj: update for this project
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -1,6 +1,26 @@
 (defproject emulator-4917 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "4917 emulator for clojure JVM and clojurescript node.js"
+  :url "https://github.com/coldnew/emulator-4917"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+
+  :source-paths ["src"]
+
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+                 [org.clojure/clojurescript "0.0-3308" :scope "provided"]]
+
+  :plugins [[lein-cljsbuild "1.0.6"]]
+
+  :min-lein-version "2.5.1"
+
+  :cljsbuild {:builds
+              [{
+                :source-paths ["src"]
+                :compiler {:output-to "target/emulator-4917.js"
+                           :output-dir "target"
+                           :source-map "target/emulator-4917.js.map"
+                           :target :nodejs
+                           :optimizations :none
+                           :pretty-print true}}]}
+  :aot :all
+  :main emulator-4917.core)
